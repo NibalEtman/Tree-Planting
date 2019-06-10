3be1ece7dcfdfeca28b78ecbf6fe33d4f5451b03
nibal <nibal.etman@gmail.com>
2019-06-10 07:28:53 -0700

    first push

---
+++ js/main.js
@@ -0,0 +1,25 @@
+/*$(document).ready(function(){
+
+    $(window).scroll(function(){
+
+        if($(window).scrollTop() > 790){
+            $(".navbar").addClass("navbar-fixed-top");
+        }
+
+        if($(window).scrollTop() < 791){
+            $(".navbar").removeClass("navbar-fixed-top");
+        }
+    });
+
+
+});*/
+
+$(document).ready(function () {
+    $(window).scroll(function () { 
+
+        if($(window).scrollTop() > 790){
+            $(".navbar").addClass("navbar-fixed-top");
+        }
+
+    });    
+});
